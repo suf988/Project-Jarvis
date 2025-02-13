2edbbe3cc3f823851c6c2bb7e677ffe19b8584da
Sufyan Ahmed <sufyanahmed49@gmail.com>
2025-02-13 15:33:41 +0100

    Added speech recognition

--- main.py
+++ main.py
@@ -11,3 +11,19 @@ def speak(text):
 
 if __name__ == "__main__":
     speak("Initializing Jarvis")
+
+    while True:
+        # listening for the wake word "Jarvis" by obtaining audio from microphone
+        try:
+            with sr.Microphone() as source:
+
+                print("Listening.....\n")
+
+                audio = recognizer.listen(source, timeout=2, phrase_time_limit=3)
+
+            print("Recognizing..... \n")
+            command = recognizer.recognize_google(audio)
+            print(f"{command}\n")
+
+        except Exception as e:
+            print(f"Error: {e}")
