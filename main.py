5d9cafe27119383a833a21ba70a0b1f6af421621
Sufyan Ahmed <sufyanahmed49@gmail.com>
2025-02-15 16:17:05 +0100

    added news telling feature in jarvis

--- main.py
+++ main.py
@@ -2,15 +2,19 @@ import speech_recognition as sr
 import webbrowser
 import pyttsx3
 from musicLibrary import music
+import requests
 
 recognizer = sr.Recognizer()
 engine = pyttsx3.init()
+newsApiKey = "f513b66ee0364db5a3593ad4f0866664"
 
 def speak(text):
     engine.say(text)
     engine.runAndWait()
 
 def processCommand(c):
+
+    # open web pages:
     if 'open google' in c:
         webbrowser.open('https://google.com')
     elif 'open facebook' in c:
@@ -19,11 +23,24 @@ def processCommand(c):
         webbrowser.open('https://youtube.com')
     elif 'open linkedin' in c:
         webbrowser.open('https://linkedin.com')
+    
+    # play music:
     elif c.startswith('play'):
         song = c.split(' ')[1]
         link = music[song]
         webbrowser.open(link)
+    
+    # tell the news:
+    elif 'news' in c:
+        r = requests.get(f"https://newsapi.org/v2/top-headlines?country=us&apiKey={newsApiKey}")
+        if r.status_code == 200:
+            data = r.json()
 
+            articles = data.get('articles', [])
+            titles = [article['title'] for article in articles]
+
+            for title in titles:
+                speak(title)
 
 if __name__ == "__main__":
     speak("Initializing Jarvis")
