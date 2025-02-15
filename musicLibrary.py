962c039c1fb2a56126bed4609236226880301a75
Sufyan Ahmed <sufyanahmed49@gmail.com>
2025-02-15 13:49:36 +0100

    added a music library

---
+++ musicLibrary.py
@@ -0,0 +1,12 @@
+music = {
+    'firworks': 'https://www.youtube.com/watch?v=QGJuMBdaqIw',
+    'bon apetite': 'https://www.youtube.com/watch?v=dPI-mRFEIH0',
+    'blank space': 'https://www.youtube.com/watch?v=e-ORhEE9VVg',
+    'calm down': 'https://www.youtube.com/watch?v=WcIcVapfqXw',
+    'despacito': 'https://www.youtube.com/watch?v=kJQP7kiw5Fk',
+    'shape of you': 'https://www.youtube.com/watch?v=JGwWNGJdvx8',
+    'closer': 'https://www.youtube.com/watch?v=PT2_F-1esPk',
+    'perfect': 'https://www.youtube.com/watch?v=2Vv-BfVoq4g',
+    'waka waka': 'https://www.youtube.com/watch?v=pRpeEdMmmQ0'
+    
+}
